f7974f30c2beff84b915f02e2e9b4caa6e2cb853
Kate <katepdonahue@gmail.com>
2014-02-07 10:07:39 -0500

    Add height and width for svg and orthographic projection: the space where we will draw the globe

---
+++ globe.js
@@ -0,0 +1,10 @@
+var width = 800,
+    height = 800;
+
+var projection = d3.geo.orthographic()
+                       .scale(350)
+                       .translate([width/2], height/2))
+                       .clipAngle(90);
+
+                       
+                        
