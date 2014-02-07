3ee049fb7f7ee5208cb4c260390845cd33be60ce
Kate <katepdonahue@gmail.com>
2014-02-07 10:18:06 -0500

    Bind the path to the svg, give it an id and pass the path as an attr

--- globe.js
+++ globe.js
@@ -6,10 +6,13 @@ var projection = d3.geo.orthographic()
                        .translate([width/2], height/2))
                        .clipAngle(90);
 
-var path = d3.geo.path().projection(projection);
+var path = d3.geo.path()
+             .projection(projection); // this makes it look 3d, it's an svg attribute
 
 var graticule = d3.geo.graticule();
 
 var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
 
-                        
+svg.append("path").datum({type: "Sphere"}) // append path and bind to datum
+                  .attr("id", "globe")
+                  .attr("d", path); // stores the path
