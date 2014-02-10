466ed2f697a3cdac668fa1ca96627168fa45c941
Kate <katepdonahue@gmail.com>
2014-02-10 10:36:37 -0500

    Get rid of graticules and add id globe

--- globe.js
+++ globe.js
@@ -13,19 +13,14 @@ var graticule = d3.geo.graticule();
 
 var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
 
-// svg.append("path").datum({type: "Sphere"}) // append path and bind to datum
-//                   .attr("id", "globe")
-//                   .attr("d", path); // stores the path
+svg.append("path").datum({type: "Sphere"}) // append path and bind to datum
+                  .attr("id", "globe")
+                  .attr("d", path); // stores the path
 
 // d3.json("data.json", function(error, world) { // world is the data from data.json
 //   svg.selectAll("path.land")
 //   .data(topojson.feature(world, world.objects.land)) // bind the data from the json file, grab the part you want, and pass it to topojson to turn it from topojson (smaller) into geojson which we need to use
 // });
-
-svg.append("path")
-   .datum(graticule)
-   .attr("class", "graticule")
-   .attr("d", path);
 
 d3.json("data.json", function(error, data) {
   svg.insert("path", ".graticule")
